f30d5b9cdd4673b2b6cbf1bf8cb1531b20e6c3ba
Christopher Field <ctafield@gmail.com>
2021-12-04 20:04:40 +0000

    part 1 (#3)
    Co-authored-by: Chris Field <chris@ctafield.com>

---
+++ aoc2021/Models/BingoCard.cs
@@ -0,0 +1,89 @@
+﻿namespace aoc2021.Models
+{
+    internal class BingoCard
+    {
+        private class Number
+        {
+            public int Value { get; set; }
+
+            public bool Marked { get; set; }
+
+            public Number(int value)
+            {
+                Value = value;
+                Marked = false;
+            }
+        }
+
+        private List<List<Number>> Rows { get; set; }
+
+        public BingoCard(string[] rows)
+        {
+            Rows = new List<List<Number>>();
+
+            foreach(var row in rows)
+            {
+                var rawNumbers = row.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var numbers = new List<Number>();
+                foreach (var rawNumber in rawNumbers)
+                {
+                    numbers.Add(new Number(int.Parse(rawNumber)));
+                }
+                
+                Rows.Add(numbers);
+            }                                   
+        }
+
+        public bool HasWon
+        {
+            get
+            {
+                foreach (var row in Rows)
+                {
+                    if (row.All(x => x.Marked))
+                        return true;
+                }
+
+                var width = Rows.First().Count();
+
+                for (var i = 0; i < width; i++)
+                {
+                    if (Rows.Select(x => x[i]).All(x => x.Marked))
+                    {
+                        return true;
+                    }
+                }
+                
+                return false;
+            }
+        }
+
+        internal void MarkNumber(int number)
+        {
+            foreach(var row in Rows)
+            {
+                foreach (var num in row)
+                {
+                    if (num.Value == number)
+                    {
+                        num.Marked = true;
+                    }
+                }
+            }
+        }
+
+        public int UnmarkedSum
+        {
+            get
+            {
+                var sum = 0;
+                foreach (var row in Rows)
+                {
+                    sum += row.Where(n => !n.Marked).Sum(n => n.Value);
+
+                }
+                return sum;
+            }
+        }
+    }
+}
