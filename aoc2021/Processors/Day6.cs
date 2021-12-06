36ad430454399d3749a59339cfbcb3acda20d31d
Chris Field <chris@ctafield.com>
2021-12-06 17:04:47 +0000

    part 2 and refactor (still has a stupid dictionary)

--- aoc2021/Processors/Day6.cs
+++ aoc2021/Processors/Day6.cs
@@ -1,67 +1,73 @@
-﻿using aoc2021.Models;
-
-namespace aoc2021.Processors
+﻿namespace aoc2021.Processors
 {
     internal class Day6 : DayBase, IDayProcessor
     {
+        public const byte ResetSpawnDays = 6;
+        public const byte DefaultChildSpawnDays = 8;
+
         public void Part1()
         {
-            var fish = LoadInput<string>("Day6.txt")
-                .ToArray()[0]
-                .Split(",")
-                .Select(x => new LanternFish(byte.Parse(x)))
-                .ToArray();
-
-            OutputDebug($"There are {fish.Count()} fish initially.");
-
-            const int days = 80;
-
-            for (var i = 0; i < days; i++)
-            {
-                foreach (var item in fish)
-                {
-                    item.ProcessDay();
-                }
-            }
-
-            long fishCount = 0;
-
-            foreach (var item in fish)
-            {
-                fishCount += item.CountFish();
-            }
-
-            OutputResult(1, fishCount);
+            ProcessFish(1, 80);
         }
 
         public void Part2()
         {
+            ProcessFish(1, 256);
+        }
+
+        private void ProcessFish(short partNumber, int days)
+        {
             var fish = LoadInput<string>("Day6.txt")
                 .ToArray()[0]
                 .Split(",")
-                .Select(x => new LanternFish(byte.Parse(x)))
-                .ToArray();
+                .Select(x => Convert.ToInt16(x))
+                .ToList();
 
             OutputDebug($"There are {fish.Count()} fish initially.");
 
-            const int days = 256;
+            var dict = new Dictionary<int, long>();
 
+            for (var i = 0; i < 8; i++)
+            {
+                dict.Add(i, fish.Count(x => x == i));
+            }
+            
             for (var i = 0; i < days; i++)
             {
-                foreach (var item in fish.AsParallel())
+                var newDict = new Dictionary<int, long>()
                 {
-                    item.ProcessDay();
+                    { 0, 0 },
+                    { 1, 0 },
+                    { 2, 0 },
+                    { 3, 0 },
+                    { 4, 0 },
+                    { 5, 0 },
+                    { 6, 0 },
+                    { 7, 0 },
+                    { 8, 0 },
+                };
+
+                for (var j = 8; j >= 0; j--)
+                {
+                    if (dict.ContainsKey(j))
+                    {
+                        newDict[j - 1] = dict[j];
+                    }
                 }
+
+                if (newDict[-1] > 0)
+                {
+                    var old = newDict[ResetSpawnDays] + newDict[-1];
+                    newDict[ResetSpawnDays] = old;
+                    newDict[DefaultChildSpawnDays] = newDict[-1];
+                }
+
+                dict = newDict;
             }
 
-            long fishCount = 0;
+            var fishCount = dict.Where(x => x.Key >= 0).Sum(x => x.Value);
 
-            foreach (var item in fish.AsParallel())
-            {
-                fishCount += item.CountFish();
-            }
-
-            OutputResult(2, fishCount);
+            OutputResult(partNumber, fishCount);
         }
     }
 }
