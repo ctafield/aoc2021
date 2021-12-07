d59b97c13fb1b97717a651cce371a9460dfc4dc1
Chris Field <chris@ctafield.com>
2021-12-07 08:52:13 +0000

    day 7

---
+++ aoc2021/Processors/Day7.cs
@@ -0,0 +1,74 @@
+﻿namespace aoc2021.Processors
+{
+    internal class Day7 : DayBase, IDayProcessor
+    {
+        public void Part1()
+        {
+            var crabs = LoadInput<string>("Day7.txt")
+                .First()
+                .Split(",")
+                .Select(x => Convert.ToInt32(x))
+                .ToArray();
+
+            var min = crabs.Min();
+            var max = crabs.Max();
+
+            var vals = new Dictionary<int, int>();
+
+            for (var i = min; i <= max; i++)
+            {
+                var sum = 0;
+
+                foreach (var c in crabs)
+                {
+                    var ma = Math.Max(i, c);
+                    var mi = Math.Min(i, c);
+                    sum += ma - mi;
+                }
+
+                vals.Add(i, sum);
+            }
+
+            var least = vals.Min(x => x.Value);
+
+            OutputResult(1, least);
+        }
+
+        public void Part2()
+        {
+            var crabs = LoadInput<string>("Day7.txt")
+                .First()
+                .Split(",")
+                .Select(x => Convert.ToInt32(x))
+                .ToArray();
+
+            var min = crabs.Min();
+            var max = crabs.Max();
+
+            var vals = new Dictionary<int, int>();
+
+            for (var i = min; i <= max; i++)
+            {
+                var sum = 0;
+
+                foreach (var c in crabs)
+                {
+                    var ma = Math.Max(i, c);
+                    var mi = Math.Min(i, c);
+
+                    for (var j = mi; j < ma; j++)
+                    {
+                        sum += j - mi + 1;
+                    }
+
+                }
+
+                vals.Add(i, sum);
+            }
+
+            var least = vals.OrderBy(x => x.Value).First().Value;
+
+            OutputResult(2, least);
+        }
+    }
+}
--- aoc2021/Program.cs
+++ aoc2021/Program.cs
@@ -7,15 +7,16 @@ class AOC2021
     {
         var processors = new List<IDayProcessor>
         {
-            //new Day1(),
-            //new Day2(),
-            //new Day3(),
-            //new Day4(),
-            //new Day5(),
+            new Day1(),
+            new Day2(),
+            new Day3(),
+            new Day4(),
+            new Day5(),
             new Day6(),
+            new Day7(),
         };
      
-        foreach(var processor in processors)
+        foreach(var processor in processors.TakeLast(1))
         {
             Console.WriteLine($"Running: {processor.GetType().Name}");
             processor.Part1();
